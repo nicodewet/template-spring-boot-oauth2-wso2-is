d461bbdbbfbaef75dce5653bd3dc5324823ef078
Nico de Wet <nico@nicodewet.com>
2016-07-21 21:18:41 +1200

    adding RequestDumpfilter to dump Tomcat traffic to the console

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,8 +1,13 @@
 package hello;
 
+import javax.servlet.Filter;
+
+import org.apache.catalina.filters.RequestDumperFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableOAuth2Sso
@@ -12,4 +17,12 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public FilterRegistrationBean requestDumperFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        Filter requestDumperFilter = new RequestDumperFilter();
+        registration.setFilter(requestDumperFilter);
+        registration.addUrlPatterns("/*");
+        return registration;
+    }
 }
